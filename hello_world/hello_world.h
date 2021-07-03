f495f650115435275df17841b8b78b8d1756da34
neudachina <neudachina@neudachinas-MacBook-Pro.local>
2021-07-03 13:02:29 +0300

    hello_world done

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -2,22 +2,36 @@
 
 #include <ostream>
 #include <string_view>
+#include <thread>
+#include <vector>
+#include <mutex>
 
 
 class HelloWorld {
  public:
   static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-  HelloWorld(size_t /*n_threads*/)
-  {
-    // Your code
+  HelloWorld(size_t n_threads) : n(n_threads) {
   }
 
-  void SayHello(std::ostream& /*os*/) {
-    // Your code
+  void SayHello(std::ostream& os) {
+      std::mutex m;
+
+      auto printer = [&] {
+          std::unique_lock lock(m);
+          os << kHelloPrefix << std::this_thread::get_id() << '\n';
+      };
+
+      for (size_t i = 0; i < n; ++i) {
+          threads.emplace_back(printer);
+      }
+
+      for (auto& t : threads)
+          t.join();
   }
 
  private:
-  // Your code
+  size_t n;
+  std::vector<std::thread> threads;
 };
 

1627dfe45ae40cf56463627398df16e5362b9f34
neudachina <neudachina@192.168.1.7>
2021-07-05 10:08:07 +0300

    hello_world fixed

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -11,27 +11,28 @@ class HelloWorld {
  public:
   static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-  HelloWorld(size_t n_threads) : n(n_threads) {
+  HelloWorld(size_t n_threads) : n_threads_(n_threads) {
   }
 
   void SayHello(std::ostream& os) {
-      std::mutex m;
+      std::mutex mutex;
 
       auto printer = [&] {
-          std::unique_lock lock(m);
+          std::unique_lock lock(mutex);
           os << kHelloPrefix << std::this_thread::get_id() << '\n';
       };
 
-      for (size_t i = 0; i < n; ++i) {
+      std::vector<std::thread> threads;
+      for (size_t i = 0; i < n_threads_; ++i) {
           threads.emplace_back(printer);
       }
 
-      for (auto& t : threads)
+      for (auto& t : threads) {
           t.join();
+      }
   }
 
  private:
-  size_t n;
-  std::vector<std::thread> threads;
+  const size_t n_threads_;
 };
 

64341080852b69a523271f79b42e896e67915971
neudachina <neudachina@neudachinas-MacBook-Pro.local>
2021-07-05 15:25:35 +0300

    reduce done

--- reduce/reduce.h
+++ reduce/reduce.h
@@ -1,11 +1,39 @@
 #pragma once
 
 #include <cstddef>
-
+#include <vector>
+#include <thread>
 
 template <typename RandomAccessIterator, class T, class Func>
-T parallel_reduce(RandomAccessIterator /*begin*/, RandomAccessIterator /*end*/,
-                  const T& /*initial_value*/, Func /*func*/, size_t /*n_threads*/) {
-  return T{};
+void reducer(RandomAccessIterator begin, RandomAccessIterator end, Func func, T* value) {
+    T result = *begin;
+    for (RandomAccessIterator it = begin + 1; it != end; ++it) {
+        result = func(result, *it);
+    }
+    *value = result;
+}
+
+template <typename RandomAccessIterator, class T, class Func>
+T parallel_reduce(RandomAccessIterator begin, RandomAccessIterator end,
+                  const T& initial_value, Func func, size_t n_threads) {
+    std::vector<T> threads_res(n_threads);
+    std::vector<std::thread> threads;
+    size_t size = (end - begin) / n_threads + ((end - begin) % n_threads != 0);
+
+    for (size_t i = 0; i < n_threads; ++i) {
+        threads.emplace_back(reducer<RandomAccessIterator, T, Func>,
+                             begin + size * i, std::min(begin + size * (i + 1), end), func, &threads_res[i]);
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    threads_res.push_back(initial_value);
+
+    T result;
+    reducer(threads_res.begin(), threads_res.end(), func, &result);
+
+    return result;
 }
 

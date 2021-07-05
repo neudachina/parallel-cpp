f6b90f559e454b4fc2ab60639a1f97f68ce807c4
neudachina <neudachina@192.168.1.7>
2021-07-05 10:41:39 +0300

    deadlock with variables named correctly

--- deadlock/deadlock.h
+++ deadlock/deadlock.h
@@ -6,28 +6,28 @@
 
 class Deadlock {
  public:
-  Deadlock() : interval(100) {
+  Deadlock() : interval_(100) {
   }
 
   void ThreadOne() {
-      std::unique_lock lock(first_mutex);
-      std::this_thread::sleep_for(interval);
-      while (!second_mutex.try_lock()) {
-          std::this_thread::sleep_for(interval);
+      std::unique_lock lock(first_mutex_);
+      std::this_thread::sleep_for(interval_);
+      while (!second_mutex_.try_lock()) {
+          std::this_thread::sleep_for(interval_);
       }
-      second_mutex.unlock();
+      second_mutex_.unlock();
   }
 
   void ThreadTwo() {
-      std::unique_lock lock(second_mutex);
-      while (!first_mutex.try_lock()) {
-          std::this_thread::sleep_for(interval);
+      std::unique_lock lock(second_mutex_);
+      while (!first_mutex_.try_lock()) {
+          std::this_thread::sleep_for(interval_);
       }
-      first_mutex.unlock();
+      first_mutex_.unlock();
   }
 
  private:
-    std::mutex first_mutex, second_mutex;
-    std::chrono::milliseconds interval;
+    std::mutex first_mutex_, second_mutex_;
+    std::chrono::milliseconds interval_;
 };
 

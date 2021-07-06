7ad46d78e18789e38e1ed2403bb15933d9e93946
neudachina <neudachina@192.168.1.7>
2021-07-06 14:28:58 +0300

    done

--- ticket_lock/ticket_lock.h
+++ ticket_lock/ticket_lock.h
@@ -1,20 +1,27 @@
 #pragma once
 
+#include <atomic>
+#include <thread>
+
 
 class TicketLock {
- public:
-  TicketLock() {
+public:
+  TicketLock() : next_ticket_(0), now_serving_(0) {
   }
 
   void Lock() {
-    // Your code
+      size_t my_ticket = next_ticket_.fetch_add(1);
+      while (now_serving_ != my_ticket) {
+          std::this_thread::yield();
+      }
+
   }
 
   void Unlock() {
-    // Your code
+      now_serving_.fetch_add(1);
   }
 
- private:
-  // Your code
+private:
+    std::atomic<size_t> next_ticket_, now_serving_;
 };
 

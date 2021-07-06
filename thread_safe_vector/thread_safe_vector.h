def5a05de06bc1aab46a548c4faf963dab0c35a1
neudachina <neudachina@192.168.1.7>
2021-07-06 11:48:09 +0300

    vector done

--- thread_safe_vector/thread_safe_vector.h
+++ thread_safe_vector/thread_safe_vector.h
@@ -1,31 +1,34 @@
 #pragma once
 
 #include <vector>
+#include <shared_mutex>
 
 
 template <typename T>
 class ThreadSafeVector {
- public:
-  ThreadSafeVector() {
-  }
+public:
+    ThreadSafeVector() = default;
 
-  T operator[](size_t index) const {
-    // Your code
-    return vector_[index];
-  }
+    T operator[](size_t index) const {
+        sh_mutex_.lock_shared();
+        T result = vector_[index];
+        sh_mutex_.unlock_shared();
+        return result;
+    }
 
-  size_t Size() const {
-    // Your code
-    return vector_.size();
-  }
+    size_t Size() const {
+        return vector_.size();
+    }
 
-  void PushBack(const T& value) {
-    // Your code
-    vector_.push_back(value);
-  }
+    void PushBack(const T& value) {
+        sh_mutex_.lock();
+        vector_.push_back(value);
+        sh_mutex_.unlock();
+    }
 
- private:
-  // Your code
-  std::vector<T> vector_;
+private:
+    mutable std::shared_mutex sh_mutex_;
+    std::vector<T> vector_;
 };
+
 

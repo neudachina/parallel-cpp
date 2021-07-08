bcf805e31f2b9aba95bb0719e4068fb16be70908
neudachina <neudachina@neudachinas-MacBook-Pro.local>
2021-07-08 22:44:29 +0300

    doesn't work

--- mcs_lock/mcs_lock.h
+++ mcs_lock/mcs_lock.h
@@ -1,20 +1,39 @@
 #pragma once
 
+#include <atomic>
+#include <thread>
+
+struct Node {
+    bool flag = false;
+    Node* next = nullptr;
+};
 
 class MCSLock {
  public:
-  MCSLock() {
-  }
+  MCSLock() : tail_(nullptr) {}
 
   void Lock() {
-    // Your code
+      Node* prev = tail_.exchange(&node_);
+      if (prev) {
+          prev->next = &node_;
+          while (!node_.flag)
+              std::this_thread::yield();
+      }
+
   }
 
   void Unlock() {
-    // Your code
+      Node* expected = &node_;
+      if (tail_.compare_exchange_weak(expected, nullptr)) {
+          return;
+      }
+      while (!node_.next)
+          std::this_thread::yield();
+      node_.next->flag = true;
   }
 
- private:
-  // Your code
+private:
+    thread_local static Node node_;
+    std::atomic<Node*> tail_;
 };
 

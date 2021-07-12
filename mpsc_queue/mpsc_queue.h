a08b35446f65da60df47726c918234334f3e5e30
neudachina <neudachina@neudachinas-MacBook-Pro.local>
2021-07-12 16:44:33 +0300

    done

--- mpsc_queue/mpsc_queue.h
+++ mpsc_queue/mpsc_queue.h
@@ -2,27 +2,53 @@
 
 #include <optional>
 
+template <typename T>
+struct Node {
+    std::atomic<T> value = T();
+    std::atomic<Node<T>*> next = nullptr;
+};
 
 template <typename T>
 class MPSCQueue {
  public:
-  MPSCQueue() {
-  }
+  MPSCQueue() : head_(nullptr) {}
 
   ~MPSCQueue() {
-    // Your code
+      while (head_) {
+          Pop();
+      }
   }
 
-  void Push(const T& /*value*/) {
-    // Your code
+  void Push(const T& value) {
+      auto node = new Node<T>;
+      node->value = value;
+      while (true) {
+          node->next = head_.load();
+          auto tmp = node->next.load();
+          if (head_.compare_exchange_weak(tmp, node))
+              return;
+      }
   }
 
   std::optional<T> Pop() {
-    return std::nullopt;
-    // Your code
+      if (head_.load() == nullptr) {
+          return std::nullopt;
+      } else {
+          Node<T>* current_head;
+          while (true) {
+              current_head = head_.load();
+              if (!current_head || head_.compare_exchange_weak(current_head, current_head->next))
+                  break;
+          }
+          Node<T>* node = current_head;
+          T res = node->value;
+          delete node;
+          return res;
+      }
   }
 
  private:
-  // Your code
+  std::atomic<Node<T>*> head_;
+
 };
 

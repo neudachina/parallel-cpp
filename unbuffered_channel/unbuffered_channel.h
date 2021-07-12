a0811d314a2dfa598c478c568da13f8923e44fad
neudachina <neudachina@neudachinas-MacBook-Pro.local>
2021-07-12 13:54:19 +0300

    doesn't work, just saving

--- unbuffered_channel/unbuffered_channel.h
+++ unbuffered_channel/unbuffered_channel.h
@@ -1,28 +1,66 @@
 #pragma once
 
 #include <optional>
+#include <condition_variable>
+#include <mutex>
+#include <iostream>
 
 
 template <typename T>
 class UnbufferedChannel{
- public:
-  UnbufferedChannel() {
-  }
+public:
+    UnbufferedChannel() = default;
 
-  void Send(const T& /*value*/) {
-    // Your code
-  }
+    void Send(const T& value) {
+        std::unique_lock lock(mutex_);
 
-  std::optional<T> Recv() {
-    // Your code
-    return std::nullopt;
-  }
+        ready_to_send_ = true;
+        message_ = &value;
+        cv2_.notify_one();
 
-  void Close() {
-    // Your code
-  }
+        while (!closed_ && !ready_to_receive_) {
+            cv1_.wait(lock);
+        }
 
- private:
-  // Your code
+        if (closed_) {
+            throw std::runtime_error("Channel is closed");
+        }
+
+        ready_to_receive_ = false;
+    }
+
+    std::optional<T> Recv() {
+        std::unique_lock lock(mutex_);
+
+        ready_to_receive_ = true;
+        std::optional<T> result = std::nullopt;
+
+        while (!closed_ && !ready_to_send_) {
+            cv2_.wait(lock);
+        }
+
+        if (closed_) {
+            return result;
+        }
+
+        result = *message_;
+        ready_to_send_ = false;
+        cv1_.notify_all();
+        return result;
+    }
+
+    void Close() {
+        closed_ = true;
+        cv1_.notify_all();
+        cv2_.notify_all();
+    }
+
+private:
+    bool closed_ = false,
+         ready_to_send_ = false,
+         ready_to_receive_ = false;
+    std::condition_variable cv1_, cv2_;
+    std::mutex mutex_;
+    T const * message_;
 };
 
